3482edd374a023960439261ddbbb64b976823d16
Christoph Neijenhuis <whateverhuis@gmail.com>
2013-06-01 17:52:48 +0200

    A lot of stuff

--- Nmbr/src/main/java/co/appeti/hack/nmbr/DownloadPhotoTask.java
+++ Nmbr/src/main/java/co/appeti/hack/nmbr/DownloadPhotoTask.java
@@ -18,11 +18,73 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 /**
  * Created by whateverhuis on 6/1/13.
  */
 public class DownloadPhotoTask extends AsyncTask<Integer, Void, File> {
+        public int[] nmbrs = { R.array.d0,
+R.array.d1,
+R.array.d2,
+R.array.d3,
+R.array.d4,
+R.array.d5,
+R.array.d6,
+R.array.d7,
+R.array.d8,
+R.array.d9,
+R.array.d10,
+R.array.d11,
+R.array.d12,
+R.array.d13,
+R.array.d14,
+R.array.d15,
+R.array.d16,
+R.array.d17,
+R.array.d18,
+R.array.d19,
+R.array.d20,
+R.array.d21,
+R.array.d22,
+R.array.d23,
+R.array.d24,
+R.array.d25,
+R.array.d26,
+R.array.d27,
+R.array.d28,
+R.array.d29,
+R.array.d30,
+R.array.d31,
+R.array.d32,
+R.array.d33,
+R.array.d34,
+R.array.d35,
+R.array.d36,
+R.array.d37,
+R.array.d38,
+R.array.d39,
+R.array.d40,
+R.array.d41,
+R.array.d42,
+R.array.d43,
+R.array.d44,
+R.array.d45,
+R.array.d46,
+R.array.d47,
+R.array.d48,
+R.array.d49,
+R.array.d50,
+R.array.d51,
+R.array.d52,
+R.array.d53,
+R.array.d54,
+R.array.d55,
+R.array.d56,
+R.array.d57,
+R.array.d58,
+R.array.d59};
+
     private EyeEmAPI api;
     private ImageView view;
 
@@ -31,48 +93,60 @@ public class DownloadPhotoTask extends AsyncTask<Integer, Void, File> {
         this.view = view;
     }
 
+    protected int randomId(int[] imgIds) {
+        Random r = new Random();
+        return imgIds[r.nextInt(imgIds.length)];
+    }
+
     @Override
-    protected File doInBackground(Integer... photoId) {
-        Integer id = photoId[0];
+    protected File doInBackground(Integer... arrId) {
+        Integer aid = arrId[0];
+        int[] imgIds = view.getResources().getIntArray(nmbrs[aid]);
+        //int id = randomId(imgIds);
+        for (int id : imgIds) {
+            if (id == 0) break;
 
-        EyeemPhotosQuery photosQuery = new EyeemPhotosQuery();
-        photosQuery.firstId = id;
-        //photosQuery.specificEndpoint = EyeemPhotosQuery.EESpecificPhotosEndpoint.EEspecificPhotosEndpointBgImages;
-        try {
-            HashMap<String, Object> map = api.getRequest(photosQuery.transformQuery());
-            EyeemPhoto photo = (EyeemPhoto) map.get("photo");
-            System.err.println(photo.photoUrl);
-            HttpURLConnection conn = (HttpURLConnection) (new URL(photo.photoUrl)).openConnection();
-            conn.setRequestMethod("GET");
-            conn.connect();
+            EyeemPhotosQuery photosQuery = new EyeemPhotosQuery();
+            photosQuery.firstId = id;
+            try {
+                HashMap<String, Object> map = api.getRequest(photosQuery.transformQuery());
+                EyeemPhoto photo = (EyeemPhoto) map.get("photo");
+                HttpURLConnection conn = (HttpURLConnection) (new URL(photo.photoUrl)).openConnection();
+                conn.setRequestMethod("GET");
+                conn.connect();
 
-            File rootdir = new File(Environment.getExternalStorageDirectory(), "nmbr");
-            File dir49 = new File(rootdir, "n49");
-            dir49.mkdirs();
-            File f = new File(dir49, photo.filename + ".jpg");
-            f.createNewFile();
-            FileOutputStream fos = new FileOutputStream(f);
+                File rootdir = new File(Environment.getExternalStorageDirectory(), "nmbr");
+                File nmbrDir = new File(rootdir, "nmbr" + aid);
+                nmbrDir.mkdirs();
+                File f = new File(nmbrDir, photo.filename + ".jpg");
+                if (f.exists())  {
+                    //System.out.println("File already loaded");
+                    break;
+                }
+                f.createNewFile();
+                FileOutputStream fos = new FileOutputStream(f);
 
-            InputStream is = conn.getInputStream();
-            byte[] buffer = new byte[2048];
-            int bytesRead = 0;
-            while ((bytesRead = is.read(buffer)) > 0) {
-                fos.write(buffer, 0, bytesRead);
+                InputStream is = conn.getInputStream();
+                byte[] buffer = new byte[2048];
+                int bytesRead = 0;
+                while ((bytesRead = is.read(buffer)) > 0) {
+                    fos.write(buffer, 0, bytesRead);
+                }
+                fos.close();
             }
-            fos.close();
+            catch (Exception e) {
+                System.err.println(e);
+                e.printStackTrace();
+            }
 
-            return f;
-        }
-        catch (Exception e) {
-            System.err.println(e);
-            e.printStackTrace();
         }
         return null;
     }
 
     @Override
     protected void onPostExecute(File f) {
-        Bitmap bm = BitmapFactory.decodeFile(f.getPath());
-        view.setImageBitmap(bm);
+//        if (f == null || f.isDirectory()) return;
+//        Bitmap bm = BitmapFactory.decodeFile(f.getPath());
+//        view.setImageBitmap(bm);
     }
 }
--- Nmbr/src/main/java/co/appeti/hack/nmbr/Nmbr.java
+++ Nmbr/src/main/java/co/appeti/hack/nmbr/Nmbr.java
@@ -1,8 +1,12 @@
 package co.appeti.hack.nmbr;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.Environment;
 import android.view.Menu;
+import android.widget.Chronometer;
 import android.widget.ImageView;
 
 import com.baseapp.eyeem.androidsdk.api.EyeEmAPI;
@@ -10,28 +14,82 @@ import com.baseapp.eyeem.androidsdk.api.EyeemConnect;
 import com.baseapp.eyeem.androidsdk.models.EyeemPhoto;
 import com.baseapp.eyeem.androidsdk.query.EyeemPhotosQuery;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Random;
 
 public class Nmbr extends EyeemConnect {
+
+    private Chronometer chrono;
+    private File rootdir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rootdir = new File(Environment.getExternalStorageDirectory(), "nmbr");
         // This is my private API key. Don't misuse it, please! :)
-        System.err.println("Querying EyeEm");
         api = new EyeEmAPI("NB0G4tTwmpQ3Ve0r6BDwxpiIdIIEhjYp");
-        DownloadPhotoTask dpt = new DownloadPhotoTask(api, (ImageView)findViewById(R.id.image));
-        dpt.execute(13559994);
+
+        for (int i = 0; i < 60; i++) {
+            DownloadPhotoTask dpt = new DownloadPhotoTask(api, (ImageView)findViewById(R.id.hour));
+            dpt.execute(i);
+        }
+        //dpt.execute(2);
+        //DownloadPhotoTask dpt2 = new DownloadPhotoTask(api, (ImageView)findViewById(R.id.minute));
+        //dpt2.execute(8);
+
+        chrono = (Chronometer) findViewById(R.id.chronometer);
+        startTimer();
     }
 
+    protected File random(File[] imgIds) {
+        Random r = new Random();
+        return imgIds[r.nextInt(imgIds.length)];
+    }
+
+    public void chooseRandomHourScreen(int min) {
+        Bitmap bm = getRandomBitmap(min);
+        ((ImageView)findViewById(R.id.hour)).setImageBitmap(bm);
+    }
+
+    public void chooseRandomMinuteScreen(int min) {
+        Bitmap bm = getRandomBitmap(min);
+         ((ImageView)findViewById(R.id.minute)).setImageBitmap(bm);
+    }
+
+    private Bitmap getRandomBitmap(int min) {
+        File nmbrDir = new File(rootdir, "nmbr" + min);
+
+        File[] files = nmbrDir.listFiles();
+        if (files == null || files.length <= 0) return null;
+        File f = random(files);
+        return BitmapFactory.decodeFile(f.getPath());
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.nmbr, menu);
         return true;
+    }
+
+    public void startTimer() {
+        chrono.start();
+        System.out.println("start timer");
+        chrono.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                System.out.println("timer tick");
+                Calendar c = Calendar.getInstance();
+                int min = c.get(Calendar.MINUTE);
+                int sec = c.get(Calendar.SECOND);
+                chooseRandomHourScreen(min);
+                chooseRandomMinuteScreen(sec);
+            }
+        });
     }
     
 }
